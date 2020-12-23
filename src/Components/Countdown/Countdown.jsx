20378d94ab2a46dcf5163fafdc149494bb7ad7b8
D3VSJ <D3VSJ@protonmail.com>
2020-12-23 13:45:58 +0000

    Minor changes to app.

--- src/Components/Countdown/Countdown.jsx
+++ src/Components/Countdown/Countdown.jsx
@@ -1,5 +1,4 @@
 const Countdown = () => {
-    // set the date we're counting down to
     var currentYear = new Date().getFullYear();
     var target_date = new Date("Dec 25, " + currentYear).getTime();
 
@@ -8,21 +7,16 @@ const Countdown = () => {
     var numD = d.getDate();
     console.log(numD)
 
-    // variables for time units
     var days, hours, minutes, seconds;
 
-    // get tag element
     var countdown = document.getElementById("daycount");
     var title = document.getElementById('title');
 
-    // update the tag with id "countdown" every 1 second
     setInterval(function () {
 
-        // find the amount of "seconds" between now and target
         var current_date = new Date().getTime();
         var seconds_left = (target_date - current_date) / 1000;
 
-        // do some time calculations
         days = parseInt(seconds_left / 86400);
         seconds_left = seconds_left % 86400;
 
